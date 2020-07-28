3d66372ebaa310c9285de6ea8d28732fa3980e32
Pranav Sinha <pran.sinha1.0@gmail.com>
2020-07-28 12:02:18 +0530

    MergeSort

--- Sorting.js
+++ Sorting.js
@@ -53,37 +53,89 @@ function bubbleSort(arr){
 
 
 function insertionSort(arr){
+    /*The array is virtually split into a sorted and an unsorted part. 
+    Values from the unsorted part are picked,
+    and inserted at the correct position in the sorted part.*/
     let sorted=[...arr];
     let n=sorted.length;
     for(let i=1;i<n;i++){
+        //selecting the elements starting from the 1st one.
         let key=sorted[i];
         let j=i-1;
         while(j>=0 && sorted[j]>key){
+            //shifting all element greater than the key,
+            //to the right by one index.
             sorted[j+1]=sorted[j];
             j-=1;
         }
+        //inserting the key at its place.
         sorted[j+1]=key;
     }
     return sorted;
+}//Time Complexity: O(n*2)
+
+
+function mergeSort(unsorted){
+    let a=[...unsorted];
+    if(a.length<=1)
+        return a;
+    
+    let mid=Math.floor(a.length/2);
+    let left=a.slice(0,mid);
+    let right=a.slice(mid);
+    return merge(mergeSort(left),mergeSort(right));
 }
+
+function merge(left,right){
+    let resultArray=[],leftIndex=0,rightIndex=0;
+    let k=0;
+    while(leftIndex<left.length && rightIndex<right.length){
+        if(left[leftIndex]<=right[rightIndex]){
+            resultArray.push(left[leftIndex]);
+            leftIndex++;
+        }
+        else{
+            resultArray.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+    while(leftIndex<left.length){
+        resultArray.push(left[leftIndex]);
+        leftIndex++;
+    }
+    while(rightIndex<right.length){
+        resultArray.push(right[rightIndex]);
+        rightIndex++;
+    }
+    return resultArray;
+}
+
+
 
 let arr=[67,87,45,69,98,123,564,77,35,56,46,84,107,125,22];
 
+// let s=performance.now();
+// let sorted=selectionSort(arr);
+// console.log(`\nsorted array: ${sorted}`);
+// let f=performance.now();
+// console.log(`\nselectionSort took ${f-s} ${((f-s)==1)?" millisecond":" milliseconds"}`)
+
+// s=performance.now();
+// sorted=bubbleSort(arr);
+// console.log(`\n\nsorted array: ${sorted}`);
+// f=performance.now();
+// console.log(`\nbubbleSort took ${f-s} ${((f-s)==1)?" millisecond":" milliseconds"}`)
+
+
+// s=performance.now();
+// sorted=insertionSort(arr);
+// console.log(`\n\nsorted array: ${sorted}`);
+// f=performance.now();
+// console.log(`\ninsertionSort took ${f-s} ${((f-s)==1)?" millisecond":" milliseconds"}`)
+
+
 let s=performance.now();
-let sorted=selectionSort(arr);
-console.log(`\nsorted array: ${sorted}`);
+let sorted=mergeSort(arr);
+console.log(`\n\nsorted array: ${sorted}`);
 let f=performance.now();
-console.log(`\nselectionSort took ${f-s} ${((f-s)==1)?" millisecond":" milliseconds"}`)
-
-s=performance.now();
-sorted=bubbleSort(arr);
-console.log(`\n\nsorted array: ${sorted}`);
-f=performance.now();
-console.log(`\nbubbleSort took ${f-s} ${((f-s)==1)?" millisecond":" milliseconds"}`)
-
-
-s=performance.now();
-sorted=insertionSort(arr);
-console.log(`\n\nsorted array: ${sorted}`);
-f=performance.now();
-console.log(`\ninsertionSort took ${f-s} ${((f-s)==1)?" millisecond":" milliseconds"}`)
+console.log(`\nmergeSort took ${f-s} ${((f-s)==1)?" millisecond":" milliseconds"}`)
