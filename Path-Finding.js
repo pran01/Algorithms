7d4bc1e58860ee1be8ff18da2c467802912c91af
Pranav Sinha <pran.sinha1.0@gmail.com>
2020-09-21 01:04:52 +0530

    Djikstra

--- Path-Finding.js
+++ Path-Finding.js
@@ -112,28 +112,36 @@ class Graph {
     this.adjList = new Map();
   }
   addVertex(v) {
-    this.adjList.set(v, []);
     this.numOfVertices++;
+    this.adjList.set(v, []);
   }
-  addEdge(v, w) {
-    this.adjList.get(v).push(w);
-    this.adjList.get(w).push(v);
+  addEdge(to, from, weight) {
+    this.adjList.get(to).push({ node: from, weight: weight });
+    this.adjList.get(from).push({ node: to, weight: weight });
+  }
+  getWeight(to, from) {
+    let dests = this.adjList.get(to);
+    for (let dest of dests) {
+      if (dest["node"] == from) {
+        return dest["weight"];
+      }
+    }
   }
   printGraph() {
     let keys = this.adjList.keys();
     for (let key of keys) {
-      let values = this.adjList.get(key);
-      let dest = "";
-      for (let value of values) {
-        dest += value + ", ";
+      let destsList = "";
+      let dests = this.adjList.get(key);
+      for (let dest of dests) {
+        destsList += `${dest["node"]}(${dest["weight"]})` + ", ";
       }
-      console.log(`${key} ==> ${dest}`);
+      console.log(`${key} ==> ${destsList}`);
     }
   }
   bfs(start) {
     let visited = new Map();
     for (let vertex of this.adjList.keys()) {
-      visited.set(vertex, false);
+      visited.set(vertex["node"], false);
     }
     let q = new Queue();
     visited.set(start, true);
@@ -144,9 +152,9 @@ class Graph {
       let dests = this.adjList.get(vertex);
       for (let i in dests) {
         let dest = dests[i];
-        if (!visited.get(dest)) {
-          visited.set(dest, true);
-          q.enqueue(dest);
+        if (!visited.get(dest["node"])) {
+          visited.set(dest["node"], true);
+          q.enqueue(dest["node"]);
         }
       }
     }
@@ -155,7 +163,7 @@ class Graph {
   dfs(start) {
     let visited = new Map();
     for (let vertex of this.adjList.keys()) {
-      visited.set(vertex, false);
+      visited.set(vertex["node"], false);
     }
     visited.set(start, true);
     this.dfsUtil(start, visited);
@@ -165,13 +173,62 @@ class Graph {
     let dests = this.adjList.get(start);
     for (let i in dests) {
       let dest = dests[i];
-      if (!visited.get(dest)) {
-        visited.set(dest, true);
-        this.dfsUtil(dest, visited);
+      if (!visited.get(dest["node"])) {
+        visited.set(dest["node"], true);
+        this.dfsUtil(dest["node"], visited);
       }
     }
   }
-  djikstraPath(s, d) {}
+  getMinNode(sp, dist) {
+    let min = Infinity;
+    let node;
+    for (let i of dist.keys()) {
+      if (sp[i] == false && dist.get(i) < min) {
+        min = dist.get(i);
+        node = i;
+      }
+    }
+    return node;
+  }
+  djikstraPath(s, d) {
+    let sp = new Map();
+    let dist = new Map();
+    let parent = new Map();
+    for (let key of this.adjList.keys()) {
+      if (key == s) dist.set(key, 0);
+      else dist.set(key, Infinity);
+    }
+    let minNode = this.getMinNode(sp, dist);
+    let count = 0;
+    while (minNode != undefined) {
+      minNode = this.getMinNode(sp, dist);
+      console.log(minNode);
+      sp.set(minNode, true);
+      let adjNodes = this.adjList.get(minNode);
+      for (let i in adjNodes) {
+        let adjNode = adjNodes[i];
+        if (
+          dist.get(minNode) + this.getWeight(minNode, adjNode) <
+          dist.get(adjNode)
+        ) {
+          dist.set(
+            adjNode,
+            dist.get(minNode) + this.getWeight(minNode, adjNode)
+          );
+          parent.set(adjNode, minNode);
+        }
+      }
+      if (minNode == d) break;
+    }
+    let path = new Stack();
+    let child = d;
+    while (child != s) {
+      path.push(child);
+      child = parent.get(child);
+    }
+    path.push(s);
+    path.display();
+  }
 }
 
 let g = new Graph();
@@ -181,14 +238,15 @@ g.addVertex("Gurgaon");
 g.addVertex("Vellore");
 g.addVertex("Coimbatore");
 g.addVertex("Mumbai");
-g.addEdge("Patna", "Delhi");
-g.addEdge("Patna", "Mumbai");
-g.addEdge("Patna", "Vellore");
-g.addEdge("Delhi", "Gurgaon");
-g.addEdge("Delhi", "Mumbai");
-g.addEdge("Vellore", "Coimbatore");
+g.addEdge("Patna", "Delhi", 1);
+g.addEdge("Patna", "Mumbai", 1);
+g.addEdge("Patna", "Vellore", 1);
+g.addEdge("Delhi", "Gurgaon", 1);
+g.addEdge("Delhi", "Mumbai", 1);
+g.addEdge("Vellore", "Coimbatore", 1);
 g.printGraph();
-console.log("-----BFS:-----");
-g.bfs("Vellore");
-console.log("-----DFS:-----");
-g.dfs("Vellore");
+// console.log("-----BFS:-----");
+// g.bfs("Vellore");
+// console.log("-----DFS:-----");
+// g.dfs("Vellore");
+g.djikstraPath("Patna", "Gurgaon");
